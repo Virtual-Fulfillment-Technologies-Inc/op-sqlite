176af016275c7c477d544be0cb6a9783c63c0df2
Kang Pyo <pyokang26@gmail.com>
2025-12-22 18:51:09 +0900

    feat(lib): add windows path to getConstants

--- src/NativeOPSQLite.ts
+++ src/NativeOPSQLite.ts
@@ -1,12 +1,15 @@
 import { TurboModuleRegistry, type TurboModule } from 'react-native';
 
 export interface Spec extends TurboModule {
-  getConstants: () => {
+  getConstants(): {
     IOS_DOCUMENT_PATH: string;
     IOS_LIBRARY_PATH: string;
     ANDROID_DATABASE_PATH: string;
     ANDROID_FILES_PATH: string;
     ANDROID_EXTERNAL_FILES_PATH: string;
+    WINDOWS_LOCAL_FOLDER: string;
+    WINDOWS_TEMP_FOLDER: string;
+    WINDOWS_DATABASE_PATH: string;
   };
 
   install(): boolean;
